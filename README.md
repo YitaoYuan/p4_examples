9de8110af841babc5d1a69a77043c378ceddb00b
YitaoYuan <2730052770@qq.com>
2023-05-03 18:29:09 +0800

    add DCQCN

--- README.md
+++ README.md
@@ -6,6 +6,13 @@
 
 二层静态转发交换机
 
+### sample_switch_red_ecn
+
+RED-ECN，也可以称作DCQCN。
+相比于sample_switch，将RDMA二对一incast流量的丢包率从1e-3级别降低到1e-5级别。
+对TCP也生效。
+推荐默认使用sample_switch_red_ecn
+
 ### traffic_mirror
 
 网络抓包器，可将网络中所有包复制到指定端口，用于网络监测
---
+++ sample_switch_red_ecn/bfrt.py
@@ -0,0 +1,10 @@
+port = [0, 180, 164, 148, 132]
+MAC = [0, 0x1070fd190095, 0x1070fd2fd851, 0x1070fd2fe441, 0x1070fd2fd421]
+for worker in range(1, len(port)):
+    bfrt.sample_switch_red_ecn.pipe.Ingress.l2_forward_table.add_with_l2_forward(MAC[worker], port[worker])
+
+bfrt.sample_switch_red_ecn.pipe.Egress.red_ecn.wred.add(0, 0, 125, 2500, 0.01)
+# DCQCN
+# 0 ~ 10KB, 0 
+# 10 ~ 200KB, 0 ~ 0.01
+# 200KB ~, 1
