4744ed982ff888a33a2bd7b32da3d810bfc7e8c4
YitaoYuan <2730052770@qq.com>
2023-07-12 13:53:20 +0800

    modify sample_switch

--- sample_switch/bfrt.py
+++ sample_switch/bfrt.py
@@ -1,6 +1,6 @@
-port = [0, 180, 164, 148, 132]
-MAC = [0, 0x1070fd190095, 0x1070fd2fd851, 0x1070fd2fe441, 0x1070fd2fd421]
-for worker in range(1, len(port)):
+port = [180, 164, 148, 132]
+MAC = [0x1070fd190095, 0x1070fd2fd851, 0x1070fd2fe441, 0x1070fd2fd421]
+for worker in range(len(port)):
     bfrt.sample_switch.pipe.Ingress.l2_forward_table.add_with_l2_forward(MAC[worker], port[worker])
 
 bfrt.sample_switch.pipe.Egress.dcqcn.wred.add(0, 0, 125, 2500, 0.01)
@@ -9,5 +9,5 @@ bfrt.sample_switch.pipe.Egress.dcqcn.wred.add(0, 0, 125, 2500, 0.01)
 # 10 ~ 200KB, 0 ~ 0.01
 # 200KB ~, 1
 
-for p in port[1:]:
+for p in port:
     bfrt.port.port.add(p, 'BF_SPEED_100G', 'BF_FEC_TYP_NONE', 4, True, 'PM_AN_FORCE_DISABLE')
